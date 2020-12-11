4a999dd704b6ec93aa1b4f9c749d315deac5c622
elcharitas <jonathanirhodia@gmail.com>
2020-12-11 04:00:33 +0100

    Refactor code

--- src/test.ts
+++ src/test.ts
@@ -1,14 +1,16 @@
 import Knuck from "./knuckjs";
 
-let app: Knuck = new Knuck(function create(this: Knuck, Route){
+new Knuck(function(this: Knuck, Route){
     // original path
     this.realpath = "/";
+
     // KnuckJS supports patterns!
     Route.pattern('fallback', '.*');
 
     // testing redirection...
     Route.redirect("/", "/home");
 
+    // testing fallback
     Route.fallback(function (path: string) {
         return `Hello Knuck! Welcome to ${path}`
     });
